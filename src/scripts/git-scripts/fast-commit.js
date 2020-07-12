14c82b93be4e9d2f4c3f6ba50be76583c84d2209
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-12 14:57:56 +0800

    2020年07月12日 14:56的快速提交

--- src/scripts/git-scripts/fast-commit.js
+++ src/scripts/git-scripts/fast-commit.js
@@ -1,17 +1,17 @@
-const ora = require("ora");
 const moment = require("moment");
 const simpleGit = require("simple-git");
+const toast = require("@/utils/toast");
 const popComfirm = require("@/utils/pop-comfirm");
 
 const git = simpleGit();
 
 module.exports = async () => {
-  const toast = ora();
   try {
     await git.add(".");
     await git.commit([moment().format("YYYY年MM月DD日 HH:ss"), "的快速提交"].join(""));
     toast.succeed("提交成功!");
     if (await popComfirm("是否推送到远程仓库?")) {
+      toast.start();
       await git.push();
     };
   } catch (error) {
