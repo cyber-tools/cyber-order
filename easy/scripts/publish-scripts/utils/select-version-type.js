50f59321d53a462d602b82f0cb9a98afe410600b
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-19 00:17:55 +0800

    1.1.0版本发布

--- easy/scripts/publish-scripts/utils/select-version-type.js
+++ easy/scripts/publish-scripts/utils/select-version-type.js
@@ -1,30 +1,25 @@
 const inquirer = require("inquirer");
-const { spawn } = require("cross-spawn");
+const { sync } = require("cross-spawn");
 
 module.exports = async () => {
-  const { action } = await inquirer.prompt({
+  const { versionType } = await inquirer.prompt({
     type: "list",
-    name: "action",
+    name: "versionType",
     default: "patch",
+    message: "选择要发布的版本类型:",
     choices: [{
       name: "大版本",
       description: "不兼容旧版的修改",
-      value: async () => {
-        spawn("npm", ["version", "major"], { stdio: "inherit" })
-      }
+      value: "major"
     }, {
       name: "小版本",
       description: "兼容旧版API的修改或功能更新",
-      value: async () => {
-        spawn("npm", ["version", "minor"], { stdio: "inherit" })
-      }
+      value: "minor"
     }, {
       name: "补丁版本",
       description: "BUG修复",
-      value: async () => {
-        spawn("npm", ["version", "patch"], { stdio: "inherit" })
-      }
+      value: "patch"
     }]
   });
-  await action();
+  await sync("npm", ["version", versionType], { stdio: "inherit" })
 };
--- easy/scripts/publish-scripts/utils/version-commit.js
+++ easy/scripts/publish-scripts/utils/version-commit.js
@@ -1,13 +1,20 @@
 const git = require("simple-git")();
+const toast = require("@/utils/toast");
 const { version } = require("@/package.json");
+const hasIgnoreFile = require("@/easy/scripts/git-scripts/utils/hasIgnoreFile");
 
 module.exports = async () => {
-  try {
-    await git.init();
-    await git.add(".");
-    await git.commit([version, "版本发布"].join(""));
-    console.log("当前版本", version);
-  } catch (error) {
-    throw error;
+  if (await hasIgnoreFile()) {
+    try {
+      await git.init();
+      await git.add(".");
+      await git.commit([version, "版本发布"].join(""));
+      console.log("当前版本", version);
+    } catch (error) {
+      throw error;
+    };
+  } else {
+    toast.warn("当前项目不存在.gitignore文件");
+    process.exit(0);
   };
 };
