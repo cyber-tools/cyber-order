dc10b5edda138be657226fb84cbce22bb716bd97
yushicheng <1542874601@qq.com>
2020-10-12 10:00:23 +0800

    1.0.2版本发布

--- cyber/scripts/git-scripts/actions/create-ignore.js
+++ cyber/scripts/git-scripts/actions/create-ignore.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-const simpleGit = require("simple-git");
 const { promisify } = require("es6-promisify");
 const toast = require("@/utils/toast");
 
 const template = require("../assets/gitignore-template");
 const hasIgnoreFile = require("../utils/hasIgnoreFile");
 
-const git = simpleGit();
 
 module.exports = async () => {
   if (await hasIgnoreFile()) {
